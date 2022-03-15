2a9fc7cf0f4edc5b319a1f955ab14427ee7d69da
rama4zis <rama.4ditya3@gmail.com>
2022-03-16 00:52:24 +0700

    Gift some simple animation hover and modif menuNav

--- assets/js/menu-items.js
+++ assets/js/menu-items.js
@@ -3,38 +3,81 @@ var menus = [
         "name": "Soto",
         "price": "10",
         "description": "Soto asli Lamongan dengan rasa yang enak, ekstra telur dan kuah yang lezat",
-        "image": "assets/img/sec-3/soto.png"
+        "image": "assets/img/sec-3/soto.png",
+        "type": "food",
     },
     {
         "name": "Rawon",
         "price": "10",
         "description": "Rawon daging sapi dengan rasa yang enak, ekstra telur dan kuah yang lezat",
-        "image": "assets/img/sec-3/rawon.jpg"
+        "image": "assets/img/sec-3/rawon.jpg",
+        "type": "food",
     },
     {
         "name": "Nasi Goreng",
         "price": "10",
         "description": "Nasi goreng ekstra nasi dengan sedikit pedas dan rasa asin",
-        "image": "assets/img/sec-3/nasgor.jpg"
+        "image": "assets/img/sec-3/nasgor.jpg",
+        "type": "food",
     },
     {
         "name": "Mie Goreng",
         "price": "10",
         "description": "Mie goreng ekstra mie dengan sedikit pedas dan rasa asin",
-        "image": "assets/img/sec-3/mie-goreng.jpg"
+        "image": "assets/img/sec-3/mie-goreng.jpg",
+        "type": "food",
     },
     {
         "name": "Bakso",
         "price": "10",
         "description": "Bakso dengan sedikit pedas dan rasa asin",
-        "image": "assets/img/sec-3/bakso.jpg"
+        "image": "assets/img/sec-3/bakso.jpg",
+        "type": "food",
     },
     {
         "name": "Nasi Ayam Jamur",
         "price": "10",
         "description": "Nasi ayam jamur dengan sedikit pedas dan rasa asin",
-        "image": "assets/img/sec-3/nasi-ayam-jamur.jpg"
+        "image": "assets/img/sec-3/nasi-ayam-jamur.jpg",
+        "type": "food",
+    },
+            // ============ DRINK ============
+    {
+        "name": "Es Teh",
+        "price": "10",
+        "description": "Es teh manis dengan kesegaran es batu murni dari pegunungan",
+        "image": "assets/img/sec-3/es-teh.jpg",
+        "type": "drink",
+    },
+    {
+        "name": "Es Jeruk",
+        "price": "10",
+        "description": "Es jeruk manis campuran gula merah asli",
+        "image": "assets/img/sec-3/es-jeruk.jpg",
+        "type": "drink",
+    },
+    {
+        "name": "Coconut Water",
+        "price": "10",
+        "description": "Coconut water dipetik langsung dari pohon pegunungan",
+        "image": "assets/img/sec-3/coconut.jpg",
+        "type": "drink",
+    },
+    {
+        "name": "Dawet Ice",
+        "price": "10",
+        "description": "Dawet ice dengan racikan cendol yang pas dan cocok untuk teman ketika makan",
+        "image": "assets/img/sec-3/dawet.jpg",
+        "type": "drink",
+    },
+    {
+        "name": "Milk Shake",
+        "price": "10",
+        "description": "Milk shake, susu yang sudah dicampur dengan racikan yang pas untuk teman ketika makan",
+        "image": "assets/img/sec-3/milk-shake.jpeg",
+        "type": "drink",
     }
+
 
 ]
 
--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,27 +1,89 @@
 import menus from './menu-items.js';
 
 var menuList = document.getElementById('menu-wrapper');
-
-document.addEventListener('DOMContentLoaded', (event) => {
-
-
-    menus.forEach( (menu) => {
+var allMenu = document.getElementById('all-menu');
+var foodBtn = document.getElementById('food-btn');
+var drinkBtn = document.getElementById('drink-btn');
 
 
+function hideAll() {
+    menuList.innerHTML = '';
+}
+
+function renderAll() {
+    // e.preventDefault();
+    console.log('all');
+    hideAll();
+    menus.forEach(menu => {
         var template = `
-            <div class="menu-item">
-                <div class="menu-img">
-                    <img src="${menu.image}" alt="${menu.name}">
-                </div>
-                <div class="menu-item-details">
-                    <h3>${menu.name}</h3>
-                    <p>${menu.description}</p>
-                    <p>Rp. ${menu.price}</p>
-                </div>
-            </div>
+        <div class="menu-item">
+        <div class="menu-img">
+        <img src="${menu.image}" alt="${menu.name}">
+        </div>
+        <div class="menu-item-details">
+        <h3>${menu.name}</h3>
+        <p>${menu.description}</p>
+        <p>Rp. ${menu.price}</p>
+        </div>
+        </div>
         `;
         menuList.innerHTML += template;
-    } );
+    });
+}
+allMenu.addEventListener('click', renderAll);
 
+function renderFood() {
+    // e.preventDefault();
+    console.log('food');
+    hideAll();
+    menus.forEach(menu => {
+        if (menu.type === 'food') {
+            var template = `
+            <div class="menu-item">
+            <div class="menu-img">
+            <img src="${menu.image}" alt="${menu.name}">
+            </div>
+            <div class="menu-item-details">
+            <h3>${menu.name}</h3>
+            <p>${menu.description}</p>
+            <p>Rp. ${menu.price}</p>
+            </div>
+            </div>
+            `;
+            menuList.innerHTML += template;
+        }
+    });
+}
+foodBtn.addEventListener('click', renderFood);
+
+
+function renderDrink() {
+    // e.preventDefault();
+    console.log('drink');
+    hideAll();
+    menus.forEach(menu => {
+        if (menu.type === 'drink') {
+            var template = `
+            <div class="menu-item">
+                <div class="menu-img">
+                <img src="${menu.image}" alt="${menu.name}">
+                </div>
+                <div class="menu-item-details">
+                <h3>${menu.name}</h3>
+                <p>${menu.description}</p>
+                <p>Rp. ${menu.price}</p>
+                </div>
+                </div>
+                `;
+            menuList.innerHTML += template;
+        }
+    });
+}
+drinkBtn.addEventListener('click', renderDrink);
+
+
+document.addEventListener('DOMContentLoaded', (e) => {
+    renderAll();
+    
     console.log("ok");
 });
